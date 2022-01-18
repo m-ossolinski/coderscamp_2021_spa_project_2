4ac1f897292c13b08cf4e13fe5182b2ce81b8e46
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2022-01-18 18:03:01 +0000

    [9][front] add header to Transaction form

--- src/pages/TransactionsPage/TransactionsForm/TransactionsForm.js
+++ src/pages/TransactionsPage/TransactionsForm/TransactionsForm.js
@@ -1,5 +1,14 @@
 import { StyledTransactionForm } from "./TransactionsForm.styled";
 
 export const TransactionForm = () => {
-  return <StyledTransactionForm>alabama</StyledTransactionForm>;
+  return (
+    <StyledTransactionForm>
+      <div>
+        <h2>Enter new transaction</h2>
+        <span>x</span>
+      </div>
+
+      <div>la</div>
+    </StyledTransactionForm>
+  );
 };
--- src/pages/TransactionsPage/TransactionsForm/TransactionsForm.styled.js
+++ src/pages/TransactionsPage/TransactionsForm/TransactionsForm.styled.js
@@ -4,4 +4,15 @@ export const StyledTransactionForm = styled.div`
   width: 500px;
   height: 500px;
   background-color: lightgreen;
+
+  & div:first-child {
+    width: 100%;
+  background-color: pink;
+  padding: 0.5rem 1rem;
+
+  display: flex;
+  justify-content: space-between;
+  }
+  & span {
+    align-self: center;
 `;
