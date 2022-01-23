4ae37dce2dc724f9168472ad3b1577167f648993
Kos Krzysztof <krzysztof.kos33@gmail.com>
2022-01-23 21:54:06 +0100

    Updated Navigation component

--- src/components/layout/Navigation/Navigation.js
+++ src/components/layout/Navigation/Navigation.js
@@ -1,8 +1,8 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 const Nav = styled.nav`
-  flex-basis: 30%;
+  flex-basis: 20%;
   display: flex;
   flex-direction: column;
 `;
@@ -11,9 +11,9 @@ const Navigation = () => {
   return (
     <div>
       <Nav>
-        <Link to="/">Home</Link>
-        <Link to="/transactions">Transactions</Link>
-        <Link to="/signup">Signup</Link>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/transactions">Transactions</NavLink>
+        <NavLink to="/signup">Signup</NavLink>
       </Nav>
     </div>
   );
