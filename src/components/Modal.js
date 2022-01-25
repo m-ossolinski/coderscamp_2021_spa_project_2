04a2145ee8bda1bd675f361209fcb9c6591f43e6
Jakub Zakrzewski <82118539+zakrzaq@users.noreply.github.com>
2022-01-25 11:48:38 +0100

    Feature/8/modals (#19)
    * inital style props base heade, contetnt
    
    * minor fix
    
    * .
    
    * buttons vunction implemented
    
    * fixed description
    
    * CR fixes-A
    
    * corrections, included proptypes
    
    * onCancel callback fixes
    
    * comments added
    
    * speeling mistake

---
+++ src/components/Modal.js
@@ -0,0 +1,50 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { StyledModal } from "./Modal.styled";
+import PropTypes from "prop-types";
+
+const Modal = ({
+  isVisible,
+  onCancel,
+  modalHeader,
+  cancelBtnLabel,
+  submitBtnLabel,
+  onSubmit,
+  children,
+}) => {
+  return isVisible
+    ? ReactDOM.createPortal(
+        <StyledModal>
+          <div className="modal-overlay" />
+          <div className="modal-wrapper">
+            <div className="modal">
+              <div className="modal-header">
+                <h3>{modalHeader ?? "Edit transaction"}</h3>
+                <button className="modal-btn-close" onClick={onCancel}>
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                {children ? <div>{children}</div> : null}
+              </div>
+              <div className="button-wrapper">
+                <button onClick={onCancel}>{cancelBtnLabel ?? "Cancel"}</button>
+                <button onClick={onSubmit}>{submitBtnLabel ?? "Submit"}</button>
+              </div>
+            </div>
+          </div>
+        </StyledModal>,
+        document.body
+      )
+    : null;
+};
+
+Modal.propTypes = {
+  children: PropTypes.node,
+  modalHeader: PropTypes.string,
+  cancelBtnLabel: PropTypes.string,
+  submitBtnLabel: PropTypes.string,
+  onSubmit: PropTypes.func,
+};
+
+export default Modal;
