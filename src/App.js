2660ad69aa2ce29346bf946c640cd04ab2b6dd29
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2022-01-31 19:49:58 +0100

    proof of concept

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { AppRoutes } from "./routes/AppRoutes";
 import { ModalExample } from "./components/ModalExample";
 import { MontlyExpenseIncome } from "./components/Charts/MontlyExpenseIncome";
+import { CategoryExpense } from "./components/Charts/CategoryExpense";
 import { CategoriesPage } from "./views/TransactionsCategories/CategpriesPage/CategoriesPage";
 import styled from "styled-components";
 
@@ -17,6 +18,7 @@ const App = () => {
         <AppRoutes />
         <ModalExample />
         <MontlyExpenseIncome />
+        <CategoryExpense />
         <CategoriesPage />
       </AppWrapper>
     </>
---
+++ src/services/api/transactionsService..js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const baseUrl = process.env.REACT_APP_TRANSACTIONS_BASE_URL;
+
+const getTransactionList = () => {
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
+
+const createTransaction = (transaction) => {
+  const request = axios.post(baseUrl, transaction);
+  return request.then((response) => response.data);
+};
+
+const updateTransaction = (id, transaction) => {
+  const request = axios.put(`${baseUrl}/${id}`, category);
+  return request.then((response) => response.data);
+};
+
+const removeTransaction = (id) => {
+  const request = axios.delete(`${baseUrl}/${id}`);
+  return request.then((response) => response.data);
+};
+
+export default {
+  getTransactionList,
+  createTransaction,
+  updateTransaction,
+  removeTransaction,
+};
