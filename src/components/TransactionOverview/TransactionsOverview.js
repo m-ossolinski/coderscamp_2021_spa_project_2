855f42810c5243c47844ab8e3e620137752acb95
sylcym <prusak.syl@gmail.com>
2022-02-01 18:42:31 +0100

    update transaction overview component

--- src/components/TransactionOverview/TransactionsOverview.js
+++ src/components/TransactionOverview/TransactionsOverview.js
@@ -2,24 +2,32 @@ import React, { useState } from "react";
 import transactions from "../../services/api/transactions.json";
 import { StyledOverview } from "./TransactionsOverview.styled";
 
-const getTotal = (type) => {
-  return transactions.filter((el) => el.type === type)
-  .map((el) => el.amount)
-  .reduce((sum, el) => sum + el, 0)
-  .toFixed(2);
-}
+const TRANSACTIONS_TYPES = {
+  expense: "expense",
+  income: "income",
+};
+
+const getTotalAmount = (type) => {
+  return transactions
+    .filter((el) => el.type === type)
+    .map((el) => el.amount)
+    .reduce((sum, el) => sum + el, 0)
+    .toFixed(2);
+};
 
 export const TransactionOverview = () => {
-  const totalExpenses = getTotal("expense");
-  const totalIncomes = getTotal("income")
-  const currentBalance = (+totalExpenses + +totalIncomes).toFixed(2);
-
   return (
     <StyledOverview>
       <h3>Transactions overview:</h3>
-      <p>Total expenses: $ {totalExpenses}</p>
-      <p>Total incomes: $ {totalIncomes}</p>
-      <p>Current balance: $ {currentBalance}</p>
+      <p>Total expenses: $ {getTotalAmount(TRANSACTIONS_TYPES.expense)}</p>
+      <p>Total incomes: $ {getTotalAmount(TRANSACTIONS_TYPES.income)}</p>
+      <p>
+        Current balance: $
+        {(
+          +getTotalAmount(TRANSACTIONS_TYPES.expense) +
+          +getTotalAmount(TRANSACTIONS_TYPES.income)
+        ).toFixed(2)}
+      </p>
       <p>Total transactions: {transactions.length}</p>
     </StyledOverview>
   );
