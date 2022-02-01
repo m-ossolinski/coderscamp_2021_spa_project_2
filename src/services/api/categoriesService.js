c137992837a23546d38fc62f25322abf32810332
sylcym <prusak.syl@gmail.com>
2022-02-01 18:43:12 +0100

    update

--- src/services/api/categoriesService.js
+++ src/services/api/categoriesService.js
@@ -1,30 +1,30 @@
-// import axios from "axios";
+import axios from "axios";
 
-// const baseUrl = process.env.REACT_APP_CATEGORIES_BASE_URL;
+const baseUrl = process.env.REACT_APP_CATEGORIES_BASE_URL;
 
-// const getCategoriesList = () => {
-//   const request = axios.get(baseUrl);
-//   return request.then((response) => response.data);
-// };
+const getCategoriesList = () => {
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
 
-// const createCategory = (category) => {
-//   const request = axios.post(baseUrl, category);
-//   return request.then((response) => response.data);
-// };
+const createCategory = (category) => {
+  const request = axios.post(baseUrl, category);
+  return request.then((response) => response.data);
+};
 
-// const updateCategory = (id, category) => {
-//   const request = axios.put(`${baseUrl}/${id}`, category);
-//   return request.then((response) => response.data);
-// };
+const updateCategory = (id, category) => {
+  const request = axios.put(`${baseUrl}/${id}`, category);
+  return request.then((response) => response.data);
+};
 
-// const removeCategory = (id) => {
-//   const request = axios.delete(`${baseUrl}/${id}`);
-//   return request.then((response) => response.data);
-// };
+const removeCategory = (id) => {
+  const request = axios.delete(`${baseUrl}/${id}`);
+  return request.then((response) => response.data);
+};
 
-// export default {
-//   getCategoriesList,
-//   createCategory,
-//   updateCategory,
-//   removeCategory,
-// };
+export default {
+  getCategoriesList,
+  createCategory,
+  updateCategory,
+  removeCategory,
+};
