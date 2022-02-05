8097bc33b857871561c0bc4d7493376853f44354
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2022-02-05 18:50:43 +0000

    resolved conflicts in pagination file

--- src/components/common/Pagination/Pagination.js
+++ src/components/common/Pagination/Pagination.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import { useEffect, useState } from "react";
-import PropTypes from "prop-types";
-import { PaginateButtons } from "../PaginateButtons/PaginateButtons";
-import { getTransactionsListLength } from "../../../services/api/transactionsListService";
-import { ButtonsList } from "./Pagination.styled";
-
-export const Pagination = ({ transactionsListSize, currentPage, paginate }) => {
-  const [transactionsListLength, setTransactionsListLength] = useState(0);
-  const [isLoading, setIsLoading] = useState(false);
-
-=======
 import PropTypes from "prop-types";
 import { PaginateButtons } from "../PaginateButtons/PaginateButtons";
 import { ButtonsList } from "./Pagination.styled";
@@ -20,7 +8,6 @@ export const Pagination = ({
   currentPage,
   paginate,
 }) => {
->>>>>>> develop
   const numberOfPages =
     transactionsListLength % transactionsListSize === 0
       ? Math.floor(transactionsListLength / transactionsListSize)
@@ -31,25 +18,6 @@ export const Pagination = ({
     (_, i) => i + 1
   );
 
-<<<<<<< HEAD
-  useEffect(async () => {
-    try {
-      setIsLoading(true);
-      const response = await getTransactionsListLength();
-      setTransactionsListLength(response);
-    } catch (error) {
-      throw new Error(error);
-    } finally {
-      setIsLoading(false);
-    }
-  }, []);
-
-  if (isLoading) {
-    return <div>Loading</div>;
-  }
-
-=======
->>>>>>> develop
   return (
     <ButtonsList>
       <PaginateButtons
@@ -64,10 +32,7 @@ export const Pagination = ({
 
 Pagination.propTypes = {
   transactionsListSize: PropTypes.number.isRequired,
-<<<<<<< HEAD
-=======
   transactionsListLength: PropTypes.number.isRequired,
->>>>>>> develop
   currentPage: PropTypes.number.isRequired,
   paginate: PropTypes.func.isRequired,
 };
