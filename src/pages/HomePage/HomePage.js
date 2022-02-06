0995606d23108eab4e8527e2804c074d7ee5b713
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2022-02-06 13:11:45 +0000

    [26] add HomePage component, changed home route in AppRoutes

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+const HomePage = () => {
+  return (
+    <>
+      <div>
+        Home Page
+        <h1>Welcome to Coders Camp 2021</h1>
+        <h2>SPA Project</h2>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,20 +1,11 @@
 import { Route, Routes } from "react-router-dom";
+import HomePage from "../pages/HomePage/HomePage";
 import TransactionsPage from "../pages/TransactionsPage/TransactionsPage";
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route
-        exact
-        path="/"
-        element={
-          <div>
-            Home Page
-            <h1>Welcome to Coders Camp 2021</h1>
-            <h2>SPA Project</h2>
-          </div>
-        }
-      />
+      <Route exact path="/" element={<HomePage />} />
       <Route exact path="/transactions" element={<TransactionsPage />} />
       <Route exact path="/stats" element={<div>Stats Page</div>} />
     </Routes>
