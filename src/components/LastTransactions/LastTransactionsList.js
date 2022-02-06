e458523a75cbf77fefde35be68e402f733601d64
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2022-02-06 15:31:01 +0000

    [26] add table with latest transactions

--- src/components/LastTransactions/LastTransactionsList.js
+++ src/components/LastTransactions/LastTransactionsList.js
@@ -1,3 +1,42 @@
 import { StyledLastTransactionsList } from "./LastTransactionsList.styled";
+import {
+  BsFillArrowDownSquareFill,
+  BsFillArrowUpRightSquareFill,
+} from "react-icons/bs";
 
-export const LastTransactionsList = () => {};
+export const LastTransactionsList = ({ transactionsList }) => {
+  const tableHeader = ["Title", "Category", "Date", "Amount"];
+  return (
+    <StyledLastTransactionsList>
+      <thead>
+        <tr>
+          {tableHeader.map((th) => (
+            <th key={th}>{th}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {transactionsList.map((t) => (
+          <tr key={t.id}>
+            <td>{t.title}</td>
+            <td>{t.category}</td>
+            <td>{t.date.split("-").reverse().join(".")}</td>
+            <td>
+              {t.type === "expense" ? (
+                <p color="red">
+                  <span>{t.amount}€</span>{" "}
+                  <BsFillArrowDownSquareFill color="#ff5454" />
+                </p>
+              ) : (
+                <p color="green">
+                  <span>{t.amount}€</span>{" "}
+                  <BsFillArrowUpRightSquareFill color="#3AD7B2" />
+                </p>
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </StyledLastTransactionsList>
+  );
+};
--- src/components/LastTransactions/LastTransactionsList.styled.js
+++ src/components/LastTransactions/LastTransactionsList.styled.js
@@ -1,3 +1,43 @@
 import styled from "styled-components";
 
-export const StyledLastTransactionsList = styled.div``;
+export const StyledLastTransactionsList = styled.table`
+  margin: 1rem;
+  font-size: 2rem;
+  height: 100%;
+  border-radius: 10px;
+  background: ${({ theme }) => theme.colors.primaryLight};
+  box-shadow: 5px 5px 12px 1px rgba(0, 0, 0, 0.13);
+
+  & thead {
+    & tr {
+      box-shadow: 5px 5px 12px 1px rgba(0,0,0,0.13);
+}
+      & th {
+        padding: 0.5rem 1rem;
+        width: 150px;
+        text-align: center;
+        &:first-child {
+          width: 200px;
+        }
+      }
+    }
+  }
+
+  & tbody {
+    border: 1px solid black;
+    & tr {
+      height: 50px;
+    }
+    & td {
+      text-align: center;
+
+      & span {
+        margin-right: 10px;
+      }
+
+      &:nth-child(3) {
+        font-size: 1.5rem;
+      }
+    }
+  }
+`;
