98a7098057150482ee52a3c10f5990d9e1b53582
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2022-02-09 19:03:25 +0000

    [77] attempt to fix heading in t5ransactions

--- src/pages/TransactionsPage/TransactionsPage/TransactionsPage.styled.js
+++ src/pages/TransactionsPage/TransactionsPage/TransactionsPage.styled.js
@@ -1,16 +1,16 @@
 import styled from "styled-components";
 
 export const TransactionPageWrapper = styled.div`
-  width: 100%;
-  height: 100%;
-  margin: 0 auto;
-  padding: 1rem;
+  padding: 2rem;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: flex-end;
 
   & p {
     margin-bottom: 2rem;
+    text-align: center;
+  }
+
+  & button {
+    align-self: center;
   }
 `;
