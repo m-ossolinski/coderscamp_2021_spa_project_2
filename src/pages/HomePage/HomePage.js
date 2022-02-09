164be9021677132e2a869855bd517c4e75cc85c0
Kosik33i6 <61019376+Kosik33i6@users.noreply.github.com>
2022-02-09 20:55:41 +0100

    Feature/70/adding transactions overwiev to home page (#78)
    * Add Transactions Overview to Home Page, Updated styles
    
    * Updated style for Transactions Overview

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,13 +2,14 @@ import { StyledHomePage } from "./HomePage.styled";
 import { LastTransactionsList } from "../../components/LastTransactions/LastTransactionsList";
 import { useContext } from "react";
 import { TransactionsListContext } from "../../services/context/TransactionsListContext";
+import { TransactionOverview } from "../../components/TransactionOverview/TransactionsOverview";
 
 const HomePage = () => {
   const { transactionsList } = useContext(TransactionsListContext);
   return (
     <StyledHomePage>
+      <TransactionOverview transactions={transactionsList} />
       <LastTransactionsList transactionsList={transactionsList} />
-      <div>widgets component</div>
     </StyledHomePage>
   );
 };
--- src/pages/HomePage/HomePage.styled.js
+++ src/pages/HomePage/HomePage.styled.js
@@ -2,7 +2,11 @@ import styled from "styled-components";
 
 export const StyledHomePage = styled.div`
   display: grid;
+  grid-template-rows: 2fr 9fr;
   grid-template-columns: 55% 45%;
+  grid-template-areas:
+    "overview overview"
+    "list list";
   gap: 2rem;
   height: 100%;
 `;
