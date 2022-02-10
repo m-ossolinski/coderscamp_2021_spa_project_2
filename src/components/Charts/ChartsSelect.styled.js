fc65c20a0f5b7d4bd7d8aa5d857da1616c6467e1
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2022-02-10 13:07:03 +0100

    1.1.1 styles

--- src/components/Charts/ChartsSelect.styled.js
+++ src/components/Charts/ChartsSelect.styled.js
@@ -1,17 +1,6 @@
 import styled from "styled-components";
 
 export const StyledSelect = styled.div`
-  *::before,
-  *::after {
-    box-sizing: border-box;
-  }
-
-  :root {
-    --select-border: #777;
-    --select-focus: blue;
-    --select-arrow: var(--select-border);
-  }
-
   select {
     // A reset of styles, including removing the default dropdown arrow
     appearance: none;
@@ -25,17 +14,7 @@ export const StyledSelect = styled.div`
     cursor: inherit;
     line-height: inherit;
     width: 250px;
-
-    // Stack above custom arrow
     z-index: 1;
-
-    // Remove dropdown arrow in IE10 & IE11
-    // @link https://www.filamentgroup.com/lab/select-css.html
-    &::-ms-expand {
-      display: none;
-    }
-
-    // Remove focus outline, will add on alternate element
     outline: none;
   }
 
@@ -44,39 +23,18 @@ export const StyledSelect = styled.div`
     grid-template-areas: "select";
     align-items: center;
     position: relative;
-    select,
-    &::after {
-      grid-area: select;
-    }
-
     min-width: 15ch;
     max-width: 30ch;
-
     border: 1px solid var(--select-border);
     border-radius: 0.25em;
     padding: 0.25em 0.5em;
-
     font-size: 1.25rem;
     cursor: pointer;
     line-height: 1.1;
-
-    // Optional styles
-    // remove for transparency
     background-color: #fff;
     background-image: linear-gradient(to top, #f9f9f9, #fff 33%);
-
-    // Custom arrow
-    &:not(.select--multiple)::after {
-      content: "";
-      justify-self: end;
-      width: 0.8em;
-      height: 0.5em;
-      background-color: var(--select-arrow);
-      clip-path: polygon(100% 0%, 0 0%, 50% 100%);
-    }
   }
 
-  // Interim solution until :focus-within has better support
   select:focus + .focus {
     position: absolute;
     top: -1px;
@@ -87,24 +45,10 @@ export const StyledSelect = styled.div`
     border-radius: inherit;
   }
 
-  select[multiple] {
-    padding-right: 0;
+  select option {
+    white-space: normal;
 
-    /*
-   * Safari will not reveal an option
-   * unless the select height has room to 
-   * show all of it
-   * Firefox and Chrome allow showing 
-   * a partial option
-   */
-    height: 6rem;
-
-    option {
-      white-space: normal;
-
-      // Only affects Chrome
-      outline-color: var(--select-focus);
-    }
+    outline-color: var(--select-focus);
   }
 
   .select--disabled {
